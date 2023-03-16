901f724ba74f540cc0dd3f91c5d3dc9d38c73b99
Ярослав Андросюк Владимирович <mokruikot02@mail.ru@mail.ru>
2023-03-16 22:11:55 +0300

    Update design

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -15,9 +17,7 @@ using System.Windows.Shapes;
 
 namespace Table_tennis
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -26,6 +26,7 @@ namespace Table_tennis
         }
 
         bool isWiden = false;
+        bool isFullScreen = false;
         private void window_initiateWiden(object sender, System.Windows.Input.MouseEventArgs e)
         {
             isWiden = true;
@@ -51,6 +52,32 @@ namespace Table_tennis
             }
         }
 
+        bool isElevate = false;
+        private void window_initiateElevate(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            isElevate = true;
+        }
+
+        private void window_endElevate(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            isElevate = false;
+
+            // Make sure capture is released.
+            Rectangle rect = (Rectangle)sender;
+            rect.ReleaseMouseCapture();
+        }
+
+        private void window_Elevate(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Rectangle rect = (Rectangle)sender;
+            if (isElevate)
+            {
+                rect.CaptureMouse();
+                double newHeight = e.GetPosition(this).Y + 0;
+                if (newHeight > 0) this.Height = newHeight;
+            }
+        }
+
         private void titleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) //Перемещение по экрану /**/
         {
             this.DragMove();
@@ -60,5 +87,31 @@ namespace Table_tennis
         {
             this.Close();
         }
+
+        private void FullScreen_Click(object sender, RoutedEventArgs e)
+        {
+            if (isFullScreen)
+            {
+                this.Width = 1920;
+                this.Height = 1080;
+                isFullScreen = false;
+                this.Top = 100;
+                this.Left = 300;
+            }
+            else
+            {
+                this.Width = System.Windows.SystemParameters.PrimaryScreenWidth - 5;
+                this.Height = System.Windows.SystemParameters.PrimaryScreenHeight - 45;
+                isFullScreen = true;
+                this.Top = 1;
+                this.Left = 1;
+            }
+        }
+
+        private void Wrap_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
     }
 }
+
